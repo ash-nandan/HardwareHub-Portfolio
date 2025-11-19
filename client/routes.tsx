a9cbb70a7fd2829ad410d9cbe2164ab1e532c415
Ash Nandan <ashr5nandan@gmail.com>
2025-11-19 13:35:35 +1300

    pushing up user profile

--- client/routes.tsx
+++ client/routes.tsx
@@ -2,13 +2,14 @@ import { createRoutesFromElements, Route } from 'react-router'
 import App from './components/App.tsx'
 import Playground from './components/Playground.tsx'
 import { SingleListing } from './components/SingleListing.tsx'
+import ProfilePage from './components/Profile.tsx'
 
 export default createRoutesFromElements(
   <>
     <Route index element={<App />} />
-
     <Route path="playground" element={<Playground />} />
     <Route path="listings" />
     <Route path="listings/:id" element={<SingleListing />} />
+    <Route path="/profile" element={<ProfilePage />} />
   </>,
 )
