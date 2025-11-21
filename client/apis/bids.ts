bac76e3831c752b2c323799735df6cf7e66c850d
Joel Tewhare <joeltewhare@gmail.com>
2025-11-22 11:45:25 +1300

    Add api function, update component with bid check for listing, if listing has bids then don't render delete button

--- client/apis/bids.ts
+++ client/apis/bids.ts
@@ -1,9 +1,15 @@
 import request from 'superagent'
-import { BidWithListing } from 'models/bids'
+import { BidCheck, BidWithListing } from 'models/bids'
 const rootURL = new URL(`/api/v1`, document.baseURI)
 
 export async function getUserBids(userId: number) {
   const res = await request.get(`${rootURL}/bids/${userId}`)
 
   return res.body as BidWithListing[]
+}
+
+export async function checkBids(listingId: number) {
+  const res = await request.get(`${rootURL}/bids/check?listingId=${listingId}`)
+
+  return res.body as BidCheck[]
 }
