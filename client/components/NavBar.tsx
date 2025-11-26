7d6275a6887cec79b2724e79b62792f166f7d705
Joel Tewhare <joeltewhare@gmail.com>
2025-11-26 22:35:33 +1300

    Add timed refetch for checking closed listings to support demo experience

--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -25,6 +25,7 @@ export default function NavBar() {
     queryKey: ['closedListings', userId],
     queryFn: () => checkClosedUserListings(userId!),
     enabled: isAuthenticated && !!userId,
+    refetchInterval: 2000, //a timed refetch every 2 seconds, to support demo
   })
 
   return (
